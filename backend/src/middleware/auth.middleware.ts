057b43f824dd9f254ab46d758ed1e3121fd8137e
subham-kr-singh <subhamkumarsingh077@gmail.com>
2026-09-11 20:45:12 +0530

    feat(backend): add authentication middleware and config

--- backend/src/middleware/auth.middleware.ts
+++ backend/src/middleware/auth.middleware.ts
@@ -1,20 +1,42 @@
 import { Request, Response, NextFunction } from 'express';
+import { getAuth, clerkClient } from '@clerk/express';
 import { AppError } from '../types/error.types.js';
-import { getAuth, clerkClient, User } from '@clerk/express';
+import { Role } from '../types/auth.type.js';
 
-export const protect = async (req: Request, res: Response, next: NextFunction) => {
+export const protect = (req: Request, res: Response, next: NextFunction) => {
   try {
     const { isAuthenticated, userId } = getAuth(req);
-    if (!isAuthenticated) {
+
+    if (!isAuthenticated || !userId) {
       const error: AppError = new Error('Unauthorized');
       error.statusCode = 401;
       return next(error);
     }
-    const user = await clerkClient.users.getUser(userId);
-    req.user = user as User;
+
+    req.userId = userId;
 
     next();
   } catch (error) {
-    return next(error);
+    next(error);
   }
+};
+
+export const authorize = (...allowedRoles: Role[]) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const user = await clerkClient.users.getUser(req.userId as string);
+
+      const role = user.publicMetadata.role as Role;
+
+      if (!allowedRoles.includes(role)) {
+        const error: AppError = new Error('Forbidden');
+        error.statusCode = 403;
+        return next(error);
+      }
+
+      next();
+    } catch (error) {
+      next(error);
+    }
+  };
 };
